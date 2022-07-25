78cbfa99bed3e89bebf6569eddf99064a3c8ddb4
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-07-25 06:35:59 -0300

    feat: adiciona uma QUERY que exibe 2 colunas e linhas das músicas mais tocadas.

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT c.cancao AS cancao,
+COUNT(h.cancao_id) AS reproducoes
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+ON c.cancao_id = h.cancao_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
